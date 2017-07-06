52243ef1f2f29852cff2f5812f59ed12bc6a7025
Kartheek Palla <palla.kartheekreddy@gmail.com>
2017-07-06 20:35:42 +0530

    Issue #478 update: unit meta added and collectioneditormeta renamed as textbook meta

--- app/scripts/contenteditor/collectioneditor-config.js
+++ app/scripts/contenteditor/collectioneditor-config.js
@@ -1,6 +1,7 @@
 org.ekstep.collectioneditor.config = {
     plugins: [
     	{ "id": "org.ekstep.lessonbrowser", "ver": "1.0", "type": "plugin" },                
-        { "id": "org.ekstep.collectioneditormeta", "ver": "1.0", "type": "plugin" }
+      { "id": "org.ekstep.textbookmeta", "ver": "1.0", "type": "plugin" },
+      { "id": "org.ekstep.unitmeta", "ver": "1.0", "type": "plugin" },
     ]
 }
