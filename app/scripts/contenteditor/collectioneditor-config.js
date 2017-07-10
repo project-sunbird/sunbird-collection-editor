07eb83c5b5fba761495dce96aff85ce66350f54a
sunil <sunils@ilimi.in>
2017-07-10 12:17:49 +0530

    Issue #1 update: collection editor

--- app/scripts/contenteditor/collectioneditor-config.js
+++ app/scripts/contenteditor/collectioneditor-config.js
@@ -3,7 +3,8 @@ org.ekstep.collectioneditor.config = {
       { "id": "org.ekstep.lessonbrowser", "ver": "1.0", "type": "plugin" },                
       { "id": "org.ekstep.textbookmeta", "ver": "1.0", "type": "plugin" },
       { "id": "org.ekstep.unitmeta", "ver": "1.0", "type": "plugin" },
-      { "id": "org.ekstep.telemetry", "ver": "1.0", "type": "plugin" }
+      { "id": "org.ekstep.telemetry", "ver": "1.0", "type": "plugin" },
+      { "id": "org.ekstep.ceheader", "ver": "1.0", "type": "plugin" }
     ],
     dispatcher: "local"
 }
