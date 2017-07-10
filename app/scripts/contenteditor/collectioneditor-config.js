048dc95605e7c35dc46a658bb2a944333d8a3d97
Kartheek Palla <palla.kartheekreddy@gmail.com>
2017-07-10 14:07:27 +0530

    Issue #478 update: collection editor meta forms

--- app/scripts/contenteditor/collectioneditor-config.js
+++ app/scripts/contenteditor/collectioneditor-config.js
@@ -3,6 +3,7 @@ org.ekstep.collectioneditor.config = {
       { "id": "org.ekstep.lessonbrowser", "ver": "1.0", "type": "plugin" },                
       { "id": "org.ekstep.textbookmeta", "ver": "1.0", "type": "plugin" },
       { "id": "org.ekstep.unitmeta", "ver": "1.0", "type": "plugin" },
+      { "id": "org.ekstep.contentmeta", "ver": "1.0", "type": "plugin" },
       { "id": "org.ekstep.telemetry", "ver": "1.0", "type": "plugin" },
       { "id": "org.ekstep.ceheader", "ver": "1.0", "type": "plugin" }
     ],
