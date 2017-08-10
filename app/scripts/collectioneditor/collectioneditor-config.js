1de8a86c562fe021d914a06116944ec984e59a8b
Kartheek Palla <palla.kartheekreddy@gmail.com>
2017-08-10 21:22:07 +0530

    Issue #1 fix:  download plugin added in confing

--- app/scripts/collectioneditor/collectioneditor-config.js
+++ app/scripts/collectioneditor/collectioneditor-config.js
@@ -1,6 +1,7 @@
 org.ekstep.contenteditor.config = _.assign(org.ekstep.contenteditor.config, {
     plugins: [
-        { "id": "org.ekstep.collectionheader", "ver": "1.0", "type": "plugin" }
+        { "id": "org.ekstep.collectionheader", "ver": "1.0", "type": "plugin" },
+        { "id": "org.ekstep.download", "ver": "1.0", "type": "plugin" }
     ]
 });
 
