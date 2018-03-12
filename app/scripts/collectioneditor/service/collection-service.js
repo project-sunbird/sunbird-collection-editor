d04c059d47004ff259799e9c65bb0a66daf8d1ec
akashgupta9990 <akash.gupta@tarento.com>
2018-03-12 16:05:46 +0530

    Issue ekstep/CE-Core-Plugins#931 fix: Added css for delete popup width

--- app/scripts/collectioneditor/service/collection-service.js
+++ app/scripts/collectioneditor/service/collection-service.js
@@ -101,7 +101,7 @@ org.ekstep.services.collectionService = new(Class.extend({
         var selectedNode = this.getActiveNode();
         if (!selectedNode.data.root) {
             ecEditor.getService('popup').open({
-                template: '<div class="ui modal active" id="deletePopup" style="top: auto;"> <div class="content"> <div class="ui grid"> <div class="ten wide column"> <span class="custom-modal-heading">Are you sure you want to delete this content?</span> </div><div class="two wide column"> <i class="close large icon four wide column right-float pointer" ng-click="closeThisDialog()"></i></div></div><p class="custom-modal-content">All content within this folder will also be deleted from this textbook.</p><button class="ui red button" ng-click="confirm()">YES, DELETE</button> </div></div>',
+                template: '<div class="ui tiny modal active" id="deletePopup" style="top: auto;"> <div class="content"> <div class="ui grid"> <div class="ten wide column"> <span class="custom-modal-heading">Are you sure you want to delete this content?</span> </div><div class="two wide column"> <i class="close large icon four wide column right-float pointer" ng-click="closeThisDialog()"></i></div></div><p class="custom-modal-content">All content within this folder will also be deleted from this textbook.</p><button class="ui red button" ng-click="confirm()">YES, DELETE</button> </div></div>',
                 controller: ["$scope", function($scope) {
                     $scope.confirm = function() {
                         var parentNode = selectedNode.getParent();
