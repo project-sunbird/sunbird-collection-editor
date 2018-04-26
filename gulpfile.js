dcb58b347847e24ee266d48fff0d49f25c8689b9
Manjunath Davanam <manjunathd@ilimi.in>
2018-04-26 13:15:38 +0530

    Issue ekstep/Collection-Editor#119 fix: Gulp file changes changed the content-editor functions plugin version from 1.2 to 1.3 ver

--- gulpfile.js
+++ gulpfile.js
@@ -207,7 +207,7 @@ gulp.task('build', ['minify','inject', 'replace', 'packageCorePlugins', 'zip']);
 var corePlugins = [
     "org.ekstep.conceptselector-1.1",
     "org.ekstep.assetbrowser-1.2",
-    "org.ekstep.contenteditorfunctions-1.2",
+    "org.ekstep.contenteditorfunctions-1.3",
     "org.ekstep.unitmeta-1.3",
     "org.ekstep.contentmeta-1.3",
     "org.ekstep.courseunitmeta-1.3",
