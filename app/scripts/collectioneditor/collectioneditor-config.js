f9776330d3f8ceb8cb6f1f9015998672a0998f37
Venkateshwaran Selvaraj <venkateshwarans@ilimi.in>
2018-07-20 17:41:11 +0530

    Issue #128 fix: updating collectioneditor-config file

--- app/scripts/collectioneditor/collectioneditor-config.js
+++ app/scripts/collectioneditor/collectioneditor-config.js
@@ -1,12 +1,9 @@
 org.ekstep.contenteditor.config = _.assign(org.ekstep.contenteditor.config, {
-    plugins: [
+    plugins: [        
         { "id": "org.ekstep.collectionheader", "ver": "1.0", "type": "plugin" },
         { "id": "org.ekstep.lessonbrowser", "ver": "1.2", "type": "plugin" },
         { "id": "org.ekstep.download", "ver": "1.0", "type": "plugin" },
-        { "id": "org.ekstep.collectionwhatsnew", "ver": "1.0", "type": "plugin" },
-        { "id": "org.ekstep.telemetry", "ver": "1.0", "type": "plugin" },
-       { "id": "org.ekstep.suggestcontent", "ver": "1.0", "type": "plugin" }
-        
+        { "id": "org.ekstep.collectionwhatsnew", "ver": "1.0", "type": "plugin" }
     ]
 });
 
