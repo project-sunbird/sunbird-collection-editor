7fc61dd8dfa4f05546e218bee6ee1934d971228a
Venkateshwaran Selvaraj <venkateshwarans@ilimi.in>
2018-11-13 10:45:03 +0530

    Issue #SB-5810 fix: special characters in units

--- app/scripts/collectioneditor/service/collection-service.js
+++ app/scripts/collectioneditor/service/collection-service.js
@@ -626,19 +626,21 @@ org.ekstep.services.collectionService = new (Class.extend({
 	},
 	removeSpecialChars: function (text) {
 		// eslint-disable-next-line
-        var iChars = "!`~@#$^*+=[]\\\'{}|\"<>%"
-		for (var i = 0; i < text.length; i++) {
-			if (iChars.indexOf(text.charAt(i)) !== -1) {
-				ecEditor.dispatchEvent('org.ekstep.toaster:error', {
-					message: 'Special characters are not allowed',
-					position: 'topCenter',
-					icon: 'fa fa-warning'
-				})
+		if(text){
+			var iChars = "!`~@#$^*+=[]\\\'{}|\"<>%"
+			for (var i = 0; i < text.length; i++) {
+				if (iChars.indexOf(text.charAt(i)) !== -1) {
+					ecEditor.dispatchEvent('org.ekstep.toaster:error', {
+						message: 'Special characters are not allowed',
+						position: 'topCenter',
+						icon: 'fa fa-warning'
+					})
+				}
 			}
-		}
-		// eslint-disable-next-line
-		text = text.replace(/[^\u0600-\u06FF\uFB50-\uFDFF\uFE70-\uFEFF\uFB50-\uFDFF\u0980-\u09FF\u0900-\u097F\u0D00-\u0D7F\u0A80-\u0AFF\u0C80-\u0CFF\u0B00-\u0B7F\u0A00-\u0A7F\u0B80-\u0BFF\u0C00-\u0C7F\w:&_\-.(\),\/\s]/g, "");
-		return text
+			// eslint-disable-next-line
+			text = text.replace(/[^\u0600-\u06FF\uFB50-\uFDFF\uFE70-\uFEFF\uFB50-\uFDFF\u0980-\u09FF\u0900-\u097F\u0D00-\u0D7F\u0A80-\u0AFF\u0C80-\u0CFF\u0B00-\u0B7F\u0A00-\u0A7F\u0B80-\u0BFF\u0C00-\u0C7F\w:&_\-.(\),\/\s]/g, "");
+			return text
+		}	
 	},
 	getObjectTypeByAddType: function (addType) {
 		var categoryList = []
