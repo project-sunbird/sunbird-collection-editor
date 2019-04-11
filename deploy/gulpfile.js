5d6bf9f6e799d9ce5b1e2db41ae6d45d2a33972c
Kartheek Palla <palla.kartheekreddy@gmail.com>
2019-04-11 21:13:07 +0530

    Issue #SB-11610 feat:  gzip bundle for collection editor

--- deploy/gulpfile.js
+++ deploy/gulpfile.js
@@ -12,6 +12,7 @@ var concat = require('gulp-concat');
 var minify = require('gulp-minifier');
 var uglify = require('gulp-uglify');
 var clean = require('gulp-clean');
+var gzip = require('gulp-gzip');
 
 var cachebust = new CacheBuster();
 gulp.task('renameminifiedfiles', function() {
@@ -57,7 +58,13 @@ gulp.task('bower-package', function() {
 
 gulp.task('package', ['iframe-package', 'embed-package', 'coreplugins-package']);
 
-gulp.task('iframe-package', ['bower-package'], function() {
+gulp.task('iframe-compress', ['bower-package'], function() {
+    return gulp.src(['build/**/*.js', 'build/**/*.css', 'build/**/*.html', 'build/**/*.png', 'build/**/*.ttf', 'build/**/*.woff', 'build/**/*.woff2'])
+    .pipe(gzip())
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('iframe-package', ['iframe-compress'], function() {
     var package_id = packageJson['name'] + '-' + 'iframe' + '-' + packageJson['version'];
     return mergeStream(gulp.src('build/**').pipe(zip(package_id + '.zip')).pipe(gulp.dest('dist/editor/')), 
     gulp.src('build/**').pipe(zip(packageJson['name'] + '-iframe-latest' + '.zip')).pipe(gulp.dest('dist/editor/')));
@@ -68,7 +75,13 @@ gulp.task('bower-package-transform', ['iframe-package'], function() {
     gulp.src('build/scripts/script.min.js').pipe(replace("src='scripts", "src='collection-editor-embed/scripts")).pipe(gulp.dest('build/scripts/')));
 });
 
-gulp.task('embed-package', ['bower-package-transform'], function() {
+gulp.task('embed-compress', ['bower-package-transform'], function() {
+    return gulp.src(['build/index.html'])
+    .pipe(gzip())
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('embed-package', ['embed-compress'], function() {
     var package_id = packageJson['name'] + '-' + 'embed' + '-' + packageJson['version'];
     return mergeStream(gulp.src('build/**').pipe(zip(package_id + '.zip')).pipe(gulp.dest('dist/editor/')),
     gulp.src('build/**').pipe(zip(packageJson['name'] + '-embed-latest' + '.zip')).pipe(gulp.dest('dist/editor/')));
